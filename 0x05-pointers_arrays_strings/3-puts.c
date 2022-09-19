fff58d8c4b384b5b696b8baf24dc8efff8065875
Maurine6 <omondimaurine713@gmail.com>
2022-09-19 02:03:41 -0700

    all

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * file:3-puts.c
+ *  _puts - Prints a string to stdout.
+ *  @str: The string to be printed.
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
