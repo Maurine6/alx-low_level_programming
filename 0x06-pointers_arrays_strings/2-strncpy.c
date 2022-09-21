87484a9ac081fb21b5e25569e07a32af87e584f9
Maurine6 <omondimaurine713@gmail.com>
2022-09-21 02:47:00 -0700

    oo6

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * file:2-strncpy.c
+ * auth- maurine o
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *@n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "A readme file on more of pointers, arrays and strings"
+echo "Welcome"
