75618b358b0922c1b7b4b94df6571b5bb6e87fb9
Maurine6 <omondimaurine713@gmail.com>
2022-09-28 05:43:03 -0700

    s

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- checks code
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	 int n;
+
+	 n = _strlen_recursion("Corbin Coleman");
+	 printf("%d\n", n);
+	 return(0);
+}
+
+
